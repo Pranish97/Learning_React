c1368fc3693d5a9471404223dd8d35b8431fa4b1
Pranish97 <pranishstha4@gmail.com>
2024-01-08 08:33:17 +0545

    Add Ternary Operators, Lists and Css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import './App.css';
+import {User} from './User';
+import {Planet} from './Planet';
 
 function App() {
+  const age = 18;
+  const isGreeen = false;
+  const users = [
+    {name:"Pranish" , age:"21"},
+    {name:"Ram" , age:"21"},
+    {name:"Hary" , age:"21"}
+
+  ];
+  const planets = [
+    {name:"Mars" , isGasPlanet:true},
+    {name:"Earth" , isGasPlanet:true},
+    {name:"Juipter" , isGasPlanet:false},
+    {name:"Venus" , isGasPlanet:true},
+    {name:"Neptune" , isGasPlanet:false},
+  ];
   
   return (
-    <div className="App">
-      <Job company="Facebook" salary={2000000} position ="Project Manager" />
-      <Job  company="Instagram" salary={5000000} position ="Senior Developer" />
-      <Job  company="Google" salary={10000000} position ="Senior Developer" />
-    </div>
+    // Ternary Operators
+    // <div className="App">
+    //   {age >= 18 ? <h1>You are in</h1> :  <h1>You are not in</h1>}
+    //   <h1 style= {{color : isGreeen ? "green" : "red"}}>This is a color</h1>
+    //   {isGreeen && <button>A button</button>}
+    // </div>
+  // );
+  
+  <div className="App">
+  {/* {planets.map((planet, key) => {
+   if(planet.isGasPlanet) return <h1>{planet.name}</h1>;
+  })} */}
+  {planets.map((planet, key) => 
+  planet.isGasPlanet && <h1>{planet.name}</h1>
+   
+  )}
+</div>
   );
 }
 
-function Job(props){
-  return (
-    <div>
-      <h1>{props.company}</h1>
-      <h2>{props.salary}</h2>
-      <h3>{props.position}</h3>
-  </div>
-  );
-};
+
+
+
+
 
 export default App;
---
+++ src/Planet.js
@@ -0,0 +1,9 @@
+export const Planet = (props)=>{
+    return(
+      <div>
+        
+     <h1>{props.name} {props.isGasPlanet}</h1>
+    </div>
+    );
+  
+  }
