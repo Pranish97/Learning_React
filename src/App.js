bf1d880e22d8899a5bdb4a31a828f8baad13c0da
Pranish97 <pranishstha4@gmail.com>
2024-01-10 10:08:14 +0545

    Adding Component Lifecycle Use Effect

--- src/App.js
+++ src/App.js
@@ -1,60 +1,22 @@
 import { useState } from 'react';
 import './App.css';
-import {Task} from "./task"
+import {Text} from "./Text"
 
 function App() {
-  const [todoList , setTodoList] = useState([]);
-  const [newTask , setNewTask] = useState("");
-
-  const handleChange = (event) =>{
-    setNewTask(event.target.value);
-  }
-
-  const addTask = () =>{
-    const task = {
-      id: todoList.length === 0 ? 1: todoList[todoList.length-1].id+1,
-      taskName: newTask,
-      complete : false,
-    };
-     setTodoList([...todoList, task]);
-  }
-  const deleteTask = (id)=>{
-    setTodoList(todoList.filter((task)=>
-     task.id !== id));
-  }
-  const completeTask = (id)=>{
-   setTodoList(
-    todoList.map((task)=>{
-      if(task.id===id){
-        return {...task, complete:true};
-      }
-      else{
-        return task;
-      }
-    })
-   )
-  }
-
-
+  const [showText, setShowText] = useState(false);
 
   return (
-  <div className="App">
-    <div className='addTask'>
-      <input onChange={handleChange}/>
-      <button onClick={addTask}>Add Task</button>
+    <div className="App">
+      <button
+        onClick={() => {
+          setShowText(!showText);
+        }}
+      >
+        Show Text
+      </button>
 
+      {showText && <Text />}
     </div>
-    <div className='list'>
-      {todoList.map((task)=>{
-        return <Task  taskName = {task.taskName} id = {task.id} 
-        deleteTask = {deleteTask} 
-        complete = {task.complete} 
-        completeTask = {completeTask} />
-       })}
-
-    </div>
-   
-</div>
   );
 }
 
