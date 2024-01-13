0b7867707b6d1bf63f04d4a4643f6366bcca4457
Pranish97 <pranishstha4@gmail.com>
2024-01-13 21:10:24 +0545

    Adding Fetching data in React

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {Home} from "./pages/Home";
-import {Contact} from "./pages/Contact";
-import {Profile} from "./pages/Profile";
-import { Navbar } from './Navbar';
-import {useState, createContext} from "react";
-
-export const AppContext = createContext();
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./Navbar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-
-  const [username,setUsername] =useState("Pranish");
-
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: true,
+      },
+    },
+  });
   return (
     <div className="App">
-      <AppContext.Provider value={{username,setUsername}}>
-      <Router>
-       <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home username={username}/>}/>
-          <Route path='/contact' element={<Contact />}/>
-          <Route path='/profile' element={<Profile username={username} setUsername={setUsername}/>}/>
-          <Route path='*' element={<h1>Page Not Found</h1>}/>
-        </Routes>
-      </Router>
-      </AppContext.Provider>
+      <QueryClientProvider client={client}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 }
-
 
 export default App;
